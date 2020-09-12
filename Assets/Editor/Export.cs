e031bbfd777753f48abaef5892198ecccb53f1c7
Sam Eller <>
2020-09-12 10:29:50 -0400

    Menu Item

---
+++ Assets/Editor/Export.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Export : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [MenuItem("Export/ExportToHTML5")]
+    public static void ExportToHTML()
+    {
+        Debug.Log("Exporting...");
+    }
+}
